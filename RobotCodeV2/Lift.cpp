12a5a2002c5011ee4e0e3bf6282cef81078532fb
Alex Tacescu <alextac98@gmail.com>
2017-04-29 22:23:01 -0400

    finished Lift, not tested

--- RobotCodeV2/Lift.cpp
+++ RobotCodeV2/Lift.cpp
@@ -1,1 +1,51 @@
+#include "Lift.h"
+#include "Pinout.h"
 
+Lift::Lift():
+      liftLeft(LIFTLEFT, isLiftLeftReversed),
+      liftRight(LIFTRIGHT, isLiftRightReversed)
+  {
+    liftPID = new PID(&liftInput, &liftOutput, &liftSetpoint, P_lift, I_lift, D_lift, DIRECT);
+    liftPID -> SetMode(AUTOMATIC); //PID disabled by default
+    liftPID -> SetSampleTime(PID_UPDATE_TIME);
+    liftPID -> SetOutputLimits(-100,100);
+  }
+void Lift::liftSetup(){
+  liftLeft.motorSetup();
+  liftRight.motorSetup();
+  calibrate();
+}
+
+void Lift::setLift(int setpoint){
+  liftSetpoint = setpoint;
+  liftInput = readPot();
+  liftPID -> Compute();
+  liftLeft.setPower(liftOutput);
+  liftRight.setPower(liftOutput);
+}
+
+void Lift::up(){
+  setLift(liftUp);
+}
+
+void Lift::down(){
+  //no speed down
+}
+
+void Lift::hold(){
+  setLift(readPot());
+}
+
+void Lift::stopAll(){
+  liftLeft.setPower(0);
+  liftRight.setPower(0);
+}
+
+int readPot(){
+  return analogRead(POT);
+}
+
+void Lift::calibrate(){
+  liftDown = readPot();
+  liftUp = liftDown + liftOffset;
+}
